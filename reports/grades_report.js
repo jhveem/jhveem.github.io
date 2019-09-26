2e9e0edf5fe42b3d59827ee4580ec9df55fe48e7
jhveem <joshua.hveem@gmail.com>
2019-09-26 16:39:03 -0600

    more

--- reports/grades_report.js
+++ reports/grades_report.js
@@ -78,7 +78,7 @@ class Student {
 		this.row = this.genRow();
     this.enrollment = {};
     this.data = {};
-    nameHTML = "<a target='_blank' href='https://btech.instructure.com/users/"+id+"'>"+this.data.sortable_name+"</a> (<a target='_blank' href='https://btech.instructure.com/courses/"+course_id+"/grades/"+id+"'>grades</a>)";
+    nameHTML = "<a target='_blank' href='https://btech.instructure.com/users/"+id+"'>"+name+"</a> (<a target='_blank' href='https://btech.instructure.com/courses/"+course_id+"/grades/"+id+"'>grades</a>)";
     this.updateCell('name', nameHTML);
     this.updateCell('section', '');
   }
