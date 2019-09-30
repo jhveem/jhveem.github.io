a0a197b4a1dae3867134c83946c1e91f706b84c6
jhveem <joshua.hveem@gmail.com>
2019-09-30 10:26:08 -0600

    and median...

--- reports/grades_report.js
+++ reports/grades_report.js
@@ -250,6 +250,7 @@ function getAssignmentData(student) {
     let perc_submitted = Math.round((submitted / max_submissions) * 100);
     student.updateCell('submissions', perc_submitted);
     updateAverage('submissions', student.dict);
+    updateMedian('submissions', student.dict);
     
     let progress = student.progress;
 
