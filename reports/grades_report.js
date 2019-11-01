27186da4a24d803984edabef07ebcbe05ba691d8
jhveem <joshua.hveem@gmail.com>
2019-11-01 10:36:10 -0600

    move from test to live

--- reports/grades_report.js
+++ reports/grades_report.js
@@ -117,10 +117,16 @@ class Student {
 		}
 		return row;
 	}
-	updateCell(key, value, color="#FFF") {
+	updateCell(key, value, url="", description="", color="#FFF") {
     let cellId = getCellId(key, this.user_id);
     let cell = $("#"+cellId);
     cell.css("background-color",color);
+    if (description !== "") {
+      value = "<div title='"+description+"'>"+value+"</div>";
+    }
+    if (url !== "") {
+      value = "<a target='_blank' style='color: #000;' href='"+url+"'>"+value+"</a>";
+    }
     
     if (columns[key].percent == true && !isNaN(parseInt(value))) value += "%";
     cell.html(value);
@@ -241,7 +247,7 @@ function getAssignmentData(student) {
       color = "#F"+g.toString(16)+"7";
     }
     if (ungraded > 10) color = "#F67";
-    student.updateCell('ungraded', ungraded, color);
+    student.updateCell('ungraded', ungraded, "", "", color);
     let perc_submitted = Math.round((submitted / max_submissions) * 100);
     if (isNaN(perc_submitted)) perc_submitted = 0;
     student.updateCell('submissions', perc_submitted);
@@ -272,14 +278,17 @@ function getAssignmentData(student) {
     }
     if (most_recent_days > 21) color = "#F67";
 
-
-    student.updateCell('days_since_last_submission', most_recent_days, color);
+    if (most_recent.assignment_id !== undefined) {
+      student.updateCell('days_since_last_submission', most_recent_days, "/courses/"+course_id+"/assignments/"+most_recent.assignment_id, "Most recent submission: " + most_recent.title, color);
+    } else {
+      student.updateCell('days_since_last_submission', most_recent_days, "", "NO SUBMISSIONS: The student may still have points if they were put in manually by the instructor or transfered from another course.", color);
+    }
     student.days_since_last_submission = most_recent_days;
     updateAverage('days_since_last_submission', student.dict);
     updateMedian('days_since_last_submission', student.dict);
   }).fail(function() {
     student.updateCell('points', "N/A");
-    student.updateCell('days_since_last_submission', "N/A", "#FAB");
+    student.updateCell('days_since_last_submission', "N/A", "", "", "#FAB");
   });
 }
 
