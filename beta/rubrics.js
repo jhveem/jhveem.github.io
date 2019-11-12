f84b8b2df2469950ba53fe280885ee2157b698eb
jhveem <joshua.hveem@gmail.com>
2019-11-12 10:39:16 -0700

    added add_javascript_library function to beta file

--- beta/rubrics.js
+++ beta/rubrics.js
@@ -1,3 +1,10 @@
+function add_javascript_library(url) {
+	var s = document.createElement("script");
+	s.setAttribute('type', 'text/javascript');
+	s.setAttribute('src', url);
+	document.getElementsByTagName('head')[0].appendChild(s);
+}
+
 add_javascript_library("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js");
 add_javascript_library("https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js");
 
