52f88ed99dacd2179182558f2c41663a6b3d2ebc
jhveem <joshua.hveem@gmail.com>
2019-11-20 12:25:13 -0700

    fonts take 3

--- custom_canvas_import.js
+++ custom_canvas_import.js
@@ -3,7 +3,7 @@ s.type = "text/javascript";
 s.src = "https://jhveem.github.io/external-libraries/sorttable.js";
 $("head").append(s);
 
-$("head").append('<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-zrnmn8R8KkWl12rAZFt4yKjxplaDaT7/EUkKm7AovijfrQItFWR7O/JJn4DAa/gx" crossorigin="anonymous"></link>');
+$("head").append('<script src="https://kit.fontawesome.com/870f83fdd7.js" crossorigin="anonymous"></script>');
 
 function addMenuItem(linkText, linkhref) {
 	'use strict';
