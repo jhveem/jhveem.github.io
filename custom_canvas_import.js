c48ed819b734631c564371656a169616c05d6ac0
jhveem <joshua.hveem@gmail.com>
2019-12-03 09:14:26 -0700

    console log for testing

--- custom_canvas_import.js
+++ custom_canvas_import.js
@@ -116,6 +116,7 @@ if (window.location.pathname == "/grades") {
 //*On speed grader page, make it so a comment is added with the rubric info whenever a rubric score is submitted
 if (/^\/courses\/[0-9]+\/gradebook\/speed_grader/.test(window.location.pathname)) {
   let user = ENV.current_user.id;
+  console.log(uers);
   if (user === 1893418) {
     $.put = function(url, data){
       return $.ajax({
