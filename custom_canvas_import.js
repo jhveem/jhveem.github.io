490a46cc7c7c4f2767070cb2dd13335a10f5b90f
jhveem <joshua.hveem@gmail.com>
2019-12-13 12:57:08 -0700

    forgot to get the length of the number of finished items, not just the items

--- custom_canvas_import.js
+++ custom_canvas_import.js
@@ -65,8 +65,7 @@ if (window.location.pathname.includes("/courses/473716/modules") === true) {
                 }
             });
             let possible = (quizzes.length + assignments.length);
-            let finished = $(value).find('i.icon-publish');
-            console.log(possible + "/" + finished);
+            let finished = $(value).find('i.icon-publish').length;
             if (possible > 0 && possible === finished) {
                 $(value).find('div.ig-header-admin').append('<span class="ig-type-icon"><i class="icon-Solid icon-publish"></i></span>');
             }
