632100bc0a1e2a97d81da8cf4072e10a07bf86cc
jhveem <joshua.hveem@gmail.com>
2020-01-22 14:59:38 -0700

    added the total criteria at max points out of total criteria

--- custom_canvas_import_beta.js
+++ custom_canvas_import_beta.js
@@ -104,18 +104,26 @@ if (/^\/courses\/[0-9]+\/assignments\/[0-9]+\/submissions\/[0-9]+/.test(window.l
   let user = parseInt(pieces[3]);
   if (user === 1893418 || rubric_courses_test.includes(course)) {
     $(".save_rubric_button").on("click", function() {
-      let comment = "<h2><b>RUBRIC</b></h2>%0A";
+      let comment = "";
+      let header = "<h2><b>RUBRIC</b></h2>%0A";
       let rows = $("div.react-rubric table").find("tr");
-      comment += ($(rows[rows.length -1]).text().trim() + "%0A%0A");
+      let totalMax = 0;
+      let totalCrit = 0;
+      header += ($(rows[rows.length -1]).text().trim() + "%0A%0A");
       $("div.react-rubric").find("tr.rubric-criterion").each(function(index) {
         let description = $(this).find("th.description-header").find("div.description").text();
         let points_val = $(this).find("td.criterion_points").find("div.graded-points").find("input").val();
         let points = $(this).find("td.criterion_points").find("div.graded-points").text();
-        console.log(points_val);
         points = points.replace("/", "").replace(" pts", "").replace("Points", "");
+        totalCrit += 1;
+        if (points === points_val) {
+          totalMax += 1;
+        }
         description = description.replace("This criterion is linked to a Learning Outcome", "");
         comment += (description + "%0A" + points_val + "/" + points + "%0A%0A");
       });
+      header += ("TOTAL" + totalMax + "/" + totalCrit);
+      comment = header + comment;
       $.put("https://btech.beta.instructure.com/api/v1/courses/"+course+"/assignments/"+assignment+"/submissions/"+user+"?comment[text_comment]="+comment,{} );
     });
   }
