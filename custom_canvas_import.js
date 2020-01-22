7121fc0f461be866a9bacf87e5e9e73020a00ef7
jhveem <joshua.hveem@gmail.com>
2020-01-22 15:28:49 -0700

    fixed regex group

--- custom_canvas_import.js
+++ custom_canvas_import.js
@@ -124,7 +124,7 @@ async function parseCommentHTML() {
     let element = await getElement(".comment, .comments");
     element.each(function() {
         var html = $(this).html();
-        html = html.replace(/&lt;[a-z]+&gt;/g, "<$1>");
+        html = html.replace(/&lt;([a-z|0-9]+)&gt;/g, "<$1>");
         $(this).html(html);
     });
 }
