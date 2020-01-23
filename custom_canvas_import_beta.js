a0da05238b18f838b38b4b49b263c78a873798e7
jhveem <joshua.hveem@gmail.com>
2020-01-23 13:00:13 -0700

    clean up comment string to see if that lets it send

--- custom_canvas_import_beta.js
+++ custom_canvas_import_beta.js
@@ -101,6 +101,9 @@ function genRubricComment(course, assignment, user, rowSelector, rubricSelector)
   header += ("TOTAL CRITERIA AT FULL POINTS: %0A" + totalMax + "/" + totalCrit);
   comment = header + '%0A<div class="btech-comment-collapse">%0A' + comment + '%0A</div>';
   console.log(course + ":" + assignment + ":" + user);
+  //clean it up to send
+  comment.replace(/</g, "&lt;");
+  comment.replace(/>/g, "&gt;");
   $.put("https://btech.beta.instructure.com/api/v1/courses/"+course+"/assignments/"+assignment+"/submissions/"+user+"?comment[text_comment]="+comment,{} );
 }
 
