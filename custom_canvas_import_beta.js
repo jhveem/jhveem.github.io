2acc388223f62315af54b9153d61f17b1eaffcd7
jhveem <joshua.hveem@gmail.com>
2020-01-23 12:46:01 -0700

    finding out why user is undefined

--- custom_canvas_import_beta.js
+++ custom_canvas_import_beta.js
@@ -109,6 +109,7 @@ if (/^\/courses\/[0-9]+\/gradebook\/speed_grader/.test(window.location.pathname)
   let currentUser = parseInt(ENV.current_user.id);
   let course = parseInt(ENV.course_id);
   let user = ENV.RUBRIC_ASSESSMENT.assessment_user_id;
+  console.log(user);
   let assignment = ENV.assignment_id;
   if (currentUser === 1893418 || rubric_courses_test.includes(course)) {
     $(".save_rubric_button").on("click", function() {
