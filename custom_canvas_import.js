16ecb4758bd736635355854ca8c81df04a972a84
jhveem <joshua.hveem@gmail.com>
2020-01-24 09:15:09 -0700

    prevent dups of toggler for comments

--- custom_canvas_import.js
+++ custom_canvas_import.js
@@ -129,11 +129,13 @@ async function parseCommentHTML() {
 
     let collapses = $(this).find('div.btech-comment-collapse');
     collapses.each(function() {
-      let criteria_id = "criteria_" + Math.round(Math.random() * 100000000);
-      let toggleHeader = '<br><h4 class="element_toggler btech-toggler" role="button" aria-controls="'+criteria_id+'" aria-expanded="false" aria-label="Toggler toggle list visibility"><i class="fal fa-comments" aria-hidden="true"></i><strong>Individual Criteria</strong></h4><br>';
-      $(this).attr("id",criteria_id);
-      $(this).css("display", "none");
-      $(toggleHeader).insertBefore(this);
+      if ($(this).find(".btech-toggler").length === 0) {}
+        let criteria_id = "criteria_" + Math.round(Math.random() * 100000000);
+        let toggleHeader = '<br><h4 class="element_toggler btech-toggler" role="button" aria-controls="'+criteria_id+'" aria-expanded="false" aria-label="Toggler toggle list visibility"><i class="fal fa-comments" aria-hidden="true"></i><strong>Individual Criteria</strong></h4><br>';
+        $(this).attr("id",criteria_id);
+        $(this).css("display", "none");
+        $(toggleHeader).insertBefore(this);
+      }
     });
   });
 }
