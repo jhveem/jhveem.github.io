8520ccc9e3dbe2fd721d71d669776617b84c15be
jhveem <joshua.hveem@gmail.com>
2020-01-28 14:06:05 -0700

    highlighting comments added to vanilla canvas

--- custom_features/highlight_comments_same_date.js
+++ custom_features/highlight_comments_same_date.js
@@ -1,40 +1,46 @@
-if (/^\/courses\/[0-9]+\/gradebook\/speed_grader/.test(window.location.pathname)) {
-  console.log("HIGHLIGHTS");
-  async function hoverByClass(classname,colorover,colorout="transparent"){
-    console.log("TEST");
-    let comments = await getElement("." + classname);
-    console.log("TEST");
-    comments.each(function() {
-        console.log("TEST");
-        console.log($(this));
-        $(this).mouseover(function() {
-            $("." + classname + "").each(function() {
-                $(this).css("background-color", colorover);
-            });
-        });
-        $(this).mouseout(function() {
-            $("." + classname + "").each(function() {
-                $(this).css("background-color", "transparent");
-            });
+async function hoverByClass(classname,colorover,colorout="transparent"){
+let comments = await getElement("." + classname);
+comments.each(function() {
+    $(this).mouseover(function() {
+        $("." + classname + "").each(function() {
+            $(this).css("background-color", colorover);
         });
     });
-  }
-  async function prepComments() {
-    let comments = await getElement("div.comment");
+    $(this).mouseout(function() {
+        $("." + classname + "").each(function() {
+            $(this).css("background-color", "transparent");
+        });
+    });
+});
+}
+async function prepComments(commentSelector) {
+    let comments = await getElement(commentSelector);
     let dates = [];
     comments.each(function() {
-        let postedAt = $(this).find("span.posted_at").text().match(/^([A-Z][a-z]+ [0-9]+)/)[1].replace(" ", "-");
-        let classname = "comment-date-" + postedAt;
-        $(this).addClass(classname);
-        if (!dates.includes(classname)) {
-            dates.push(classname);
+    let postedAtElement = $(this).find("span.posted_at");
+    if (postedAtElement.length === 1) {
+        let postedAtGroups = postedAtElement.text().match(/^([A-Z][a-z]+ [0-9]+)/);
+        if (postedAtGroups !== null) {
+            console.log(postedAtGroups);
+            let postedAt = postedAtGroups[1].replace(" ", "-");
+            let classname = "comment-date-" + postedAt;
+            $(this).addClass(classname);
+            if (!dates.includes(classname)) {
+                dates.push(classname);
+            }
         }
+    }
     });
     for (var d = 0; d < dates.length; d++) {
         let date = dates[d];
-        console.log(date);
         hoverByClass(date, "#FFC");
     }
-  }
-  prepComments();
+}
+
+if (/^\/courses\/[0-9]+\/gradebook\/speed_grader/.test(window.location.pathname)) {
+  prepComments("div.comment");
+}
+
+if (/^\/courses\/[0-9]+\/assignments\/[0-9]+\/submissions\/[0-9]+/.test(window.location.pathname)) {
+  prepComments("div.comment_list div.comment");
 }
