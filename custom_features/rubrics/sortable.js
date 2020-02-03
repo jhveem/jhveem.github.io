fd7f6c9f9ae6c423d8f4fe385e3ff8a59fdf7673
jhveem <joshua.hveem@gmail.com>
2020-02-03 12:49:31 -0700

    reworked sortable rubric so it will not add two buttons

--- custom_features/rubrics/sortable.js
+++ custom_features/rubrics/sortable.js
@@ -75,8 +75,9 @@ if (window.location.pathname.includes("/rubrics") === true || window.location.pa
       $('table.rubric_table tbody').children('tr').each(function(element) {
         let toolList = $(this).find('td.criterion_description div.editing');
         let criteriaDescription = $(this).find('td.criterion_description span.description_title').text();
-        toolList.find('button').remove();
-        toolList.append('<button class="fill_ratings">+</button>');
+        toolList.find('button.fill_ratings').remove();
+        let deletIcon = toolList.find("a.delete_criterion_link");
+        deleteIcon.after('<button class="fill_ratings">+</button>');
         toolList.find('.fill_ratings').click({parent: this, criteria: criteriaDescription}, function(event) {
           let parent = event.data.parent;
           let criteriaDescription = event.data.criteria;
