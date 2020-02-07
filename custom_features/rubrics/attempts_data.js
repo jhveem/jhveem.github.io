678c9d9cd7102b6f7699d8263951d660ff1b85c1
jhveem <joshua.hveem@gmail.com>
2020-02-07 16:29:01 -0700

    wrapping features in anonymous function

--- custom_features/rubrics/attempts_data.js
+++ custom_features/rubrics/attempts_data.js
@@ -1,56 +1,58 @@
 //SPEED GRADER
-let rWindowSpeedGrader = /^\/courses\/[0-9]+\/gradebook\/speed_grader/;
-let rWindowVanilla = /^\/courses\/[0-9]+\/assignments\/[0-9]+\/submissions\/[0-9]+/;
-if (rWindowSpeedGrader.test(window.location.pathname) && rWindowSpeedGrader.test(window.location.pathname)) {
-  IMPORTED_FEATURE = {
-    attempts: 0,
-    initiated: false,
-    commentSelector: "",
-    _init(params={}) {
-      let feature = this;
-      if (rWindowSpeedGrader.test(window.location.pathname)) {
-        feature.commentSelector = "div.comment span.comment, tr.comments";
-      }
-      if (rWindowVanilla.test(window.location.pathname)) {
-        feature.commentSelector = "div.comment div.comment span";
-      }
-      feature.insertAttemptsData();
-      $(".save_rubric_button").on("click", function() {
+(function() {
+  let rWindowSpeedGrader = /^\/courses\/[0-9]+\/gradebook\/speed_grader/;
+  let rWindowVanilla = /^\/courses\/[0-9]+\/assignments\/[0-9]+\/submissions\/[0-9]+/;
+  if (rWindowSpeedGrader.test(window.location.pathname) && rWindowSpeedGrader.test(window.location.pathname)) {
+    IMPORTED_FEATURE = {
+      attempts: 0,
+      initiated: false,
+      commentSelector: "",
+      _init(params={}) {
+        let feature = this;
+        if (rWindowSpeedGrader.test(window.location.pathname)) {
+          feature.commentSelector = "div.comment span.comment, tr.comments";
+        }
+        if (rWindowVanilla.test(window.location.pathname)) {
+          feature.commentSelector = "div.comment div.comment span";
+        }
+        feature.insertAttemptsData();
+        $(".save_rubric_button").on("click", function() {
+          feature.calcAttemptsData();
+          feature.attempts += 1;
+        });
+      },
+      async insertAttemptsData() {
+        let feature = this;
+        let details = await getElement("#rubric_holder tbody.criterions");
+        details.after(
+          `<tr id="btech-attempts-data" class="content_box">
+          <td colspan="3">
+          <div id="btech-recorded-attempts"><b>Recorded Attempts:</b> <span id="btech-recorded-attempts-value"></span></div>
+          <div id="btech-rubric-score"><b>Rubric Score:</b> <span id="btech-rubric-score-value"></span></div>
+          <div id="btech-suggested-score"><b>Suggested Score:</b> <span id="btech-suggested-score-value"></span></div>
+          </td>
+          </tr>`);
         feature.calcAttemptsData();
-        feature.attempts += 1;
-      });
-    },
-    async insertAttemptsData() {
-      let feature = this;
-      let details = await getElement("#rubric_holder tbody.criterions");
-      details.after(
-        `<tr id="btech-attempts-data" class="content_box">
-        <td colspan="3">
-        <div id="btech-recorded-attempts"><b>Recorded Attempts:</b> <span id="btech-recorded-attempts-value"></span></div>
-        <div id="btech-rubric-score"><b>Rubric Score:</b> <span id="btech-rubric-score-value"></span></div>
-        <div id="btech-suggested-score"><b>Suggested Score:</b> <span id="btech-suggested-score-value"></span></div>
-        </td>
-        </tr>`);
-      feature.calcAttemptsData();
-    },
-    async calcAttemptsData() {
-      let feature = this;
-      feature.attempts = 0;
-      let elements = await getElement(feature.commentSelector);
-      elements.each(function() {
-          let checkAttempt = $(this).html().includes("RUBRIC");
-          if (checkAttempt) {
-              feature.attempts += 1;
-          }
-      });
-      if (feature.attempts > 0) {
-        let rubricTotal = $("[data-selenium='rubric_total']").text();
-        rubricTotal = parseInt(rubricTotal.match(/([0-9]+)/)[1]);
-        let suggestedScore = Math.round(rubricTotal * ((11 - feature.attempts) / 10));
-        $("#btech-recorded-attempts-value").text(feature.attempts);
-        $("#btech-rubric-score-value").text(rubricTotal);
-        $("#btech-suggested-score-value").text(suggestedScore);
+      },
+      async calcAttemptsData() {
+        let feature = this;
+        feature.attempts = 0;
+        let elements = await getElement(feature.commentSelector);
+        elements.each(function() {
+            let checkAttempt = $(this).html().includes("RUBRIC");
+            if (checkAttempt) {
+                feature.attempts += 1;
+            }
+        });
+        if (feature.attempts > 0) {
+          let rubricTotal = $("[data-selenium='rubric_total']").text();
+          rubricTotal = parseInt(rubricTotal.match(/([0-9]+)/)[1]);
+          let suggestedScore = Math.round(rubricTotal * ((11 - feature.attempts) / 10));
+          $("#btech-recorded-attempts-value").text(feature.attempts);
+          $("#btech-rubric-score-value").text(rubricTotal);
+          $("#btech-suggested-score-value").text(suggestedScore);
+        }
       }
     }
   }
-}
+})();
--- custom_features/rubrics/gen_comment.js
+++ custom_features/rubrics/gen_comment.js
@@ -1,58 +1,60 @@
-let rWindowSpeedGrader = /^\/courses\/([0-9]+)\/gradebook\/speed_grader/;
-let rWindowVanilla = /^\/courses\/([0-9]+)\/assignments\/([0-9]+)\/submissions\/([0-9]+)/;
-if (rWindowSpeedGrader.test(window.location.pathname) && rWindowSpeedGrader.test(window.location.pathname)) {
-  IMPORTED_FEATURE = {
-    courseId: null,
-    assignmentId: null,
-    studentId: null,
-    _init() {
-      let feature = this;
-      if (rWindowSpeedGrader.test(window.location.pathname)) {
-        feature.courseId = parseInt(ENV.course_id);
-        feature.studentId = ENV.RUBRIC_ASSESSMENT.assessment_user_id;
-        feature.assignmentId = ENV.assignment_id;
-      }
+(function() {
+  let rWindowSpeedGrader = /^\/courses\/([0-9]+)\/gradebook\/speed_grader/;
+  let rWindowVanilla = /^\/courses\/([0-9]+)\/assignments\/([0-9]+)\/submissions\/([0-9]+)/;
+  if (rWindowSpeedGrader.test(window.location.pathname) && rWindowSpeedGrader.test(window.location.pathname)) {
+    IMPORTED_FEATURE = {
+      courseId: null,
+      assignmentId: null,
+      studentId: null,
+      _init() {
+        let feature = this;
+        if (rWindowSpeedGrader.test(window.location.pathname)) {
+          feature.courseId = parseInt(ENV.course_id);
+          feature.studentId = ENV.RUBRIC_ASSESSMENT.assessment_user_id;
+          feature.assignmentId = ENV.assignment_id;
+        }
 
-      if (rWindowVanilla.test(window.location.pathname)) {
-        let pieces = window.location.pathname.match(rWindowVanilla);
-        feature.courseId = parseInt(pieces[1]);
-        feature.studentId = parseInt(pieces[3]);
-        feature.assignmentId = parseInt(pieces[2]);
+        if (rWindowVanilla.test(window.location.pathname)) {
+          let pieces = window.location.pathname.match(rWindowVanilla);
+          feature.courseId = parseInt(pieces[1]);
+          feature.studentId = parseInt(pieces[3]);
+          feature.assignmentId = parseInt(pieces[2]);
+        }
+        $(".save_rubric_button").on("click", function() {
+          feature.genRubricComment(feature.courseId, feature.assignmentId, feature.studentId, "div#rubric_full", "div#rubric_full", 2);
+        });
+      },
+      genRubricComment(course, assignment, user, rowSelector, rubricSelector, offset=1) {
+        let comment = "";
+        let header = "<h2><b>RUBRIC</b></h2>";
+        let rows = $(rowSelector).find("tr");
+        let totalMax = 0;
+        let totalCrit = 0;
+        header += ($("#rubric_holder").find("[data-selenium='rubric_total']").text() + "\n");
+        $(rubricSelector).find("tr.rubric-criterion").each(function(index) {
+          let description = $(this).find("th.description-header").find("div.description").text();
+          let points_val = $(this).find("td.criterion_points").find("div.graded-points").find("input").val();
+          let points = $(this).find("td.criterion_points").find("div.graded-points").text();
+          points = points.replace("/", "").replace(" pts", "").replace("Points", "");
+          totalCrit += 1;
+          points = ("" + points).trim();
+          points_val = ("" + points_val).trim();
+          if (points === points_val) {
+            totalMax += 1;
+          }
+          description = description.replace("This criterion is linked to a Learning Outcome", "");
+          comment += (description + "\n" + points_val + "/" + points + "\n");
+        });
+        header += ("Total Criteria at Full Points: " + totalMax + "/" + totalCrit);
+        comment = header + '\n<div class="btech-comment-collapse">\n' + comment + '\n</div>';
+        let url = "/api/v1/courses/"+course+"/assignments/"+assignment+"/submissions/"+user;
+        console.log(url);
+        $.put(url,{
+          comment:{
+            text_comment: comment
+          }
+        });
       }
-      $(".save_rubric_button").on("click", function() {
-        feature.genRubricComment(feature.courseId, feature.assignmentId, feature.studentId, "div#rubric_full", "div#rubric_full", 2);
-      });
-    },
-    genRubricComment(course, assignment, user, rowSelector, rubricSelector, offset=1) {
-      let comment = "";
-      let header = "<h2><b>RUBRIC</b></h2>";
-      let rows = $(rowSelector).find("tr");
-      let totalMax = 0;
-      let totalCrit = 0;
-      header += ($("#rubric_holder").find("[data-selenium='rubric_total']").text() + "\n");
-      $(rubricSelector).find("tr.rubric-criterion").each(function(index) {
-        let description = $(this).find("th.description-header").find("div.description").text();
-        let points_val = $(this).find("td.criterion_points").find("div.graded-points").find("input").val();
-        let points = $(this).find("td.criterion_points").find("div.graded-points").text();
-        points = points.replace("/", "").replace(" pts", "").replace("Points", "");
-        totalCrit += 1;
-        points = ("" + points).trim();
-        points_val = ("" + points_val).trim();
-        if (points === points_val) {
-          totalMax += 1;
-        }
-        description = description.replace("This criterion is linked to a Learning Outcome", "");
-        comment += (description + "\n" + points_val + "/" + points + "\n");
-      });
-      header += ("Total Criteria at Full Points: " + totalMax + "/" + totalCrit);
-      comment = header + '\n<div class="btech-comment-collapse">\n' + comment + '\n</div>';
-      let url = "/api/v1/courses/"+course+"/assignments/"+assignment+"/submissions/"+user;
-      console.log(url);
-      $.put(url,{
-        comment:{
-          text_comment: comment
-        }
-      });
     }
   }
-}
+})();
