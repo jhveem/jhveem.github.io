f575f3e1c50895652ff50849dea216b4e04e4171
jhveem <joshua.hveem@gmail.com>
2020-02-13 08:55:14 -0700

    feature did not save

---
+++ custom_features/modules/points_to_hours_header.js
@@ -0,0 +1,43 @@
+(function() {
+  IMPORTED_FEATURE = {};
+  if (/^\/courses\/[0-9]+\/modules$/.test(window.location.pathname)) {
+      IMPORTED_FEATURE = {
+          initiated: false,
+          _init() {
+              let modules = $('div.item-group-condensed');
+              let hoursTotal = 0;
+              modules.each(function() {
+                  let headerElement = $($(this).find("div.ig-header span.name")[0]);
+                  let header = headerElement.text().trim();
+                  let headerMatch = header.match(/(.*) - [0-9]+[\.]{0,1}[0-9]* [H|h]our(s){0,1}/);
+                  if (headerMatch !== null) {
+                      header = headerMatch[1].trim();
+                  }
+                  if (header !== "") {
+                      console.log(header);
+                      let items = $(this).find("ul.ig-list div.ig-row");
+                      let pointsTotal = 0;
+                      items.each(function() {
+                          let info = $(this).find('div.ig-info');
+                          let pointsElement = info.find('span.points_possible');
+                          if (pointsElement.text().trim() !== "") {
+                              let points = parseInt(pointsElement.text());
+                              pointsTotal += points;
+                          }
+                      });
+                      let hours = pointsTotal / 10;
+                      hoursTotal += hours;
+                      let hoursString = "Hour";
+                      if (hours !== 1) {
+                          hoursString += "s";
+                      }
+                      $(headerElement.text(header + " - " + hours + " " + hoursString + " (TOTAL: " + hoursTotal + ")"));
+                  }
+              });
+          }
+      }
+      if (ENV.current_user_roles.includes("teacher")) {
+          IMPORTED_FEATURE._init();
+      }
+  }
+})();
