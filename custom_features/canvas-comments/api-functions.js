2d4022278e523f17747d6544916c6ba811a4e438
jhveem <joshua.hveem@gmail.com>
2020-02-28 14:42:41 -0700

    test getting of settings

--- custom_features/canvas-comments/api-functions.js
+++ custom_features/canvas-comments/api-functions.js
@@ -119,16 +119,9 @@ CANVAS_COMMENTS_API = {
 
   },
   async saveSettingGeneral(userId, setting, val) {
-    let url = "/api/v1/users/"+userId+"/custom_data/canvas_collaboration/general";
-    let data = {};
-    data[setting] = val;
+    let url = "/api/v1/users/"+userId+"/custom_data/canvas_collaboration/general/"+setting+"?ns=edu.btech.canvas-app&data="+val;
     try {
-      await axios.put(url, {}, {
-        params: {
-          ns: 'edu.btech.canvas-app',
-          data: data 
-        },
-      });
+      await axios.put(url);
     } catch (e) {
       console.log(e);
     }
