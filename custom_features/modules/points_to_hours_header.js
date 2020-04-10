9b3f01d427c003900707773ca62ae9f88e9833b1
jhveem <joshua.hveem@gmail.com>
2020-04-10 10:03:31 -0600

    changed the points to hours code to be conditional on a specific text header existing

--- custom_features/modules/points_to_hours_header.js
+++ custom_features/modules/points_to_hours_header.js
@@ -1,44 +1,69 @@
 (function () {
   IMPORTED_FEATURE = {};
   if (/^\/courses\/[0-9]+\/modules$/.test(window.location.pathname)) {
-    IMPORTED_FEATURE = {
-      initiated: false,
-      _init() {
-        let modules = $('div.item-group-condensed');
-        let hoursTotal = 0;
-        modules.each(function () {
-          let headerElement = $($(this).find("div.ig-header span.name")[0]);
-          let header = headerElement.text().trim();
-          let headerMatch = header.match(/(.*) - [0-9]+[\.]{0,1}[0-9]* [H|h]our(s){0,1}/);
-          if (headerMatch !== null) {
-            header = headerMatch[1].trim();
-          }
-          if (header !== "") {
-            let items = $(this).find("ul.ig-list div.ig-row");
-            let pointsTotal = 0;
-            items.each(function () {
-            if ($(this).hasClass('ig-published')) {
-                let info = $(this).find('div.ig-info');
-                let pointsElement = info.find('span.points_possible');
-                if (pointsElement.text().trim() !== "") {
-                  let points = parseFloat(pointsElement.text());
-                  pointsTotal += points;
-                }
+      IMPORTED_FEATURE = {
+          initiated: false,
+          displayHours: false,
+          displayHoursElement: {},
+          hoursTotal: 0,
+          _init() {
+              let feature = this;
+              let modules = $('div.item-group-condensed');
+              let hoursTotal = 0;
+              modules.each(function () {
+                  let headerElement = $($(this).find("div.ig-header span.name")[0]);
+                  let header = headerElement.text().trim();
+                  if (header !== "") {
+                      let items = $(this).find("ul.ig-list div.ig-row");
+                      items.each(function () {
+                          let label = $(this).find("a").attr("aria-label");
+                          if (label.includes("#HOURSTOTAL#")) {
+                              feature.displayHoursElement = this;
+                              feature.displayHours = true;
+                          }
+                      });
+                  }
+              });
+              if (feature.displayHours) {
+                  modules.each(function () {
+                      let module = $(this);
+                      let headerElement = $(headerElements[0]);
+                      let header = headerElement.text().trim();
+                      let headerMatch = header.match(/(.*) - [0-9]+[\.]{0,1}[0-9]* [H|h]our(s){0,1}/);
+                      if (headerMatch !== null) {
+                          header = headerMatch[1].trim();
+                      }
+                      if (header !== "") {
+                          let items = module.find("ul.ig-list div.ig-row");
+                          let pointsTotal = 0;
+                          items.each(function () {
+                              if ($(this).hasClass('ig-published')) {
+                                  let info = $(this).find('div.ig-info');
+                                  let pointsElement = info.find('span.points_possible');
+                                  if (pointsElement.text().trim() !== "") {
+                                      let points = parseFloat(pointsElement.text());
+                                      pointsTotal += points;
+                                  }
+                              }
+                          });
+                          let hours = pointsTotal / 10;
+                          hoursTotal += hours;
+                          let hoursString = "Hour";
+                          if (hours != 1) {
+                              hoursString += "s";
+                          }
+                          headerElements.each(function() {
+                              let oElement = $(this);
+                              let displayElement = oElement.clone();
+                              oElement.after(displayElement);
+                              oElement.hide();
+                              displayElement.text(header + " - " + hours.toFixed(1) + " " + hoursString + " (Total: " + hoursTotal + ")");
+                          });
+                      }
+                  });
               }
-            });
-            let hours = pointsTotal / 10;
-            hoursTotal += hours;
-            let hoursString = "Hour";
-            if (hours != 1) {
-              hoursString += "s";
-            }
-            $(headerElement.text(header + " - " + hours.toFixed(1) + " " + hoursString + " (Total: " + hoursTotal + ")"));
+              $(feature.displayHoursElement).find('span.title').text($(feature.displayHoursElement).find('span.title').text().replace("#HOURSTOTAL#", hoursTotal));
           }
-        });
       }
-    }
-    if (ENV.current_user_roles.includes("teacher")) {
-      IMPORTED_FEATURE._init();
-    }
   }
 })();
