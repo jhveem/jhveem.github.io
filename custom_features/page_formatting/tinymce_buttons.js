60076e1d1148564cdd6a9a7ab16dc0d3b3407c26
jhveem <joshua.hveem@gmail.com>
2020-04-16 08:23:21 -0600

    removed broken code from tinymice buttons

--- custom_features/page_formatting/tinymce_buttons.js
+++ custom_features/page_formatting/tinymce_buttons.js
@@ -1,21 +1,25 @@
 async function getEditor() {
-    console.log(window.tinymce);
-    if (window.tinymce === undefined) {
-        await delay(500);
-        return getEditor();
-    } else {
-        return;
-    }
+  console.log(window.tinymce);
+  if (window.tinymce === undefined) {
+    await delay(500);
+    return getEditor();
+  } else {
+    var editor;
+    if (editors.wiki_page_body !== undefined) editor = editors.wiki_page_body;
+    else if (editors.assignment_description !== undefined) editor = editors.assignment_description;
+    return editor;
+  }
 }
-function hideOnHover() {
-    let editor = window.tinymce.editors.wiki_page_body;
-    let selection = editor.selection;
-    editor.execCommand("mceReplaceContent", false, "<span class='btech-hover-show'><i>{$selection}</i></span>");
+async function hideOnHover() {
+  let editor = await getEditor();
+  let selection = editor.selection;
+  editor.execCommand("mceReplaceContent", false, "<span class='btech-hover-show'><i>{$selection}</i></span>");
 }
+
 function exampleBox() {
-    let editor = window.tinymce.editors.wiki_page_body;
-    let selection = editor.selection;
-    editor.execCommand("mceReplaceContent", false, `<table class="btech-example-table" style="width: 90%; border-collapse: collapse; border-color: gray; margin-left: auto; margin-right: auto; height: 62px;" border="0" cellpadding="10">
+  let editor = await getEditor();
+  let selection = editor.selection;
+  editor.execCommand("mceReplaceContent", false, `<table class="btech-example-table" style="width: 90%; border-collapse: collapse; border-color: gray; margin-left: auto; margin-right: auto; height: 62px;" border="0" cellpadding="10">
 <tbody>
 <tr style="background-color: #d22232;">
 <td style="width: 2.5%; height: 32px;"><span style="font-size: 14pt;"><strong><span style="color: #ffffff;">&nbsp;</span></strong></span></td>
@@ -34,14 +38,14 @@ function exampleBox() {
 }
 async function addButton(name, func) {
   let topPart = await getElement(".mce-top-part");
-  let button = $("<a class='btn' style='padding: 5px; background-color: #EEE; border: 1px solid #AAA; cursor: pointer;'>"+name+"</a>");
+  let button = $("<a class='btn' style='padding: 5px; background-color: #EEE; border: 1px solid #AAA; cursor: pointer;'>" + name + "</a>");
   button.click(func);
   topPart.after(button);
 }
 async function _init() {
-  await getEditor();
-  window.tinymce.editors.wiki_page_body.addShortcut("ctrl+alt+h", "The highlighted font will be hidden until the reader highlights it.", hideOnHover);
-  window.tinymce.editors.wiki_page_body.addShortcut("ctrl+alt+e", "the highlighted font will be put inside of an emphasis box.", exampleBox);
+  let editor = await getEditor();
+  editor.addShortcut("ctrl+alt+h", "The highlighted font will be hidden until the reader highlights it.", hideOnHover);
+  editor.addShortcut("ctrl+alt+e", "the highlighted font will be put inside of an emphasis box.", exampleBox);
   addButton("Hover Text", hideOnHover);
   addButton("Example Box", exampleBox);
 }
