5096c00d427cccdb8ca3b3992142c0c0aeb1602b
jhveem <joshua.hveem@gmail.com>
2020-04-16 08:50:35 -0600

    added in color selector

--- custom_features/page_formatting/tinymce_buttons.js
+++ custom_features/page_formatting/tinymce_buttons.js
@@ -19,9 +19,10 @@ async function hideOnHover() {
 async function exampleBox() {
   let editor = await getEditor();
   let selection = editor.selection;
+  let color = $("#btech-custom-editor-buttons-color").children("option:selected").val();
   editor.execCommand("mceReplaceContent", false, `<table class="btech-example-table" style="width: 90%; border-collapse: collapse; border-color: gray; margin-left: auto; margin-right: auto; height: 62px;" border="0" cellpadding="10">
 <tbody>
-<tr style="background-color: #d22232;">
+<tr style="background-color: `+color+`;">
 <td style="width: 2.5%; height: 32px;"><span style="font-size: 14pt;"><strong><span style="color: #ffffff;">&nbsp;</span></strong></span></td>
 <td style="width: 95%; height: 32px;"><span style="font-size: 14pt;"><strong><span style="color: #ffffff;">&nbsp;Example</span></strong></span></td>
 <td style="width: 2.5%; height: 32px;"><span style="font-size: 14pt;"><strong><span style="color: #ffffff;">&nbsp;</span></strong></span></td>
@@ -37,15 +38,19 @@ async function exampleBox() {
 </table>`);
 }
 async function addButton(name, func) {
-  let topPart = await getElement(".mce-top-part");
+  let customButtonsContainer = $("#btech-custom-editor-buttons-container");
   let button = $("<a class='btn' style='padding: 5px; background-color: #EEE; border: 1px solid #AAA; cursor: pointer;'>" + name + "</a>");
   button.click(func);
-  topPart.after(button);
+  customButtonsContainer.append(button);
 }
 async function _init() {
   let editor = await getEditor();
   editor.addShortcut("ctrl+alt+h", "The highlighted font will be hidden until the reader highlights it.", hideOnHover);
   editor.addShortcut("ctrl+alt+e", "the highlighted font will be put inside of an emphasis box.", exampleBox);
+  let topPart = await getElement(".mce-top-part");
+  topPart.after("<div id='btech-custom-editor-buttons-container'></div>");
+  let customButtonsContainer = $("#btech-custom-editor-buttons-container");
+  customButtonsContainer.prepend("<select id='btech-custom-editor-buttons-color' name='colors'><option value='#d22232'>Red</option><option value='#2232d2'>Blue</option></select>")
   addButton("Hover Text", hideOnHover);
   addButton("Example Box", exampleBox);
 }
