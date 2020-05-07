4d786396f35a3f227adc4cf5256c0d8cb9cf37d9
jhveem <joshua.hveem@gmail.com>
2020-05-07 09:09:15 -0600

    trouble shooting why buttons no longer appear

--- custom_features/page_formatting/tinymce_buttons.js
+++ custom_features/page_formatting/tinymce_buttons.js
@@ -106,9 +106,9 @@ async function _init() {
   console.log(editor);
   let topPart = null;
   console.log(tinymce);
-  if (tinymce.majorVersion === 4) {
+  if (tinymce.majorVersion === "4") {
     topPart = await getElement(".mce-top-part");
-  } else if (tinymce.majorVersion === 5) {
+  } else if (tinymce.majorVersion === "5") {
     topPart = await getElement(".tox-toolbar-overlord");
   }
   if ( topPart !== null && $("#btech-custom-editor-buttons-container").length === 0) {
