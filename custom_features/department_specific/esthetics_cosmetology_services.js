d3f97ea966f16178cb8e948d265a9f7f8a7e798e
jhveem <joshua.hveem@gmail.com>
2020-05-15 14:33:51 -0600

    remove student view

--- custom_features/department_specific/esthetics_cosmetology_services.js
+++ custom_features/department_specific/esthetics_cosmetology_services.js
@@ -229,54 +229,5 @@ COMMENT: ` + comment + `
           }
         }
       }
-
-      //SUBMISSION VIEW
-    } else if (/^\/courses\/[0-9]+\/assignments\/[0-9]+/.test(window.location.pathname)) {
-      //Just add in a div with the id:  btech-services-modal
-      //Change this so they just have to type #SERVICES#, no ids or anything like that
-      IMPORTED_FEATURE = {
-        initiated: false, //SET TO TRUE WHEN feature() IS RUN FROM THE custom_canvas.js PAGE TO MAKE SURE FEATURE ISN'T INITIATED TWICE
-        _init(params = {}) { //SOME FEATURES NEED CUSTOM PARAMS DEPENDING ON THE USER/DEPARTMENT/COURSE SUCH AS IF DENTAL HAS ONE SET OF RULES GOVERNING FORMATTING WHILE BUSINESS HAS ANOTHER
-          let contents = $("div.user_content").html();
-          $("div.user_content").html(contents.replace("#SERVICES#", "<div id='btech-services-modal'></div>"));
-          $("#btech-services-modal").empty();
-          $("#btech-services-modal").append("<div id='btech-services-dropdown'></div><div id='btech-services-submit'>Submit</div>");
-          let dropdownContainer = $('#btech-services-dropdown');
-          dropdownContainer.empty();
-          dropdownContainer.append(`
-<select></select>
-`);
-          let select = dropdownContainer.find('select');
-          $("#rubrics table.rubric_table tr.criterion").each(function () {
-            if ($(this).attr('id') !== 'criterion_blank') {
-              let title = $(this).find("td.criterion_description div.description_content span.description_title").text();
-              select.append(`<option value="` + title + `">` + title + `</option>`);
-            }
-          });
-          $("#btech-services-submit").css({
-            background: '#D00004',
-            cursor: 'pointer',
-            align: 'center',
-            'text-align': 'center',
-            width: '150px',
-            'border-radius': '2px',
-            'color': '#FFFFFF'
-          });
-          $("#btech-services-submit").click(function () {
-            let id = Math.floor(Date.now() / 1000);
-            let comment = "ID: " + id + "\nTYPE: submission\nSERVICE: " + select.val();
-            let url = "/api/v1/courses/" + ENV.COURSE_ID + "/assignments/" + ENV.ASSIGNMENT_ID + "/submissions/" + ENV.current_user_id;
-            $.put(url, {
-              comment: {
-                text_comment: comment
-              }
-            });
-            $("#btech-services-modal").empty();
-            $("#btech-services-modal").append(`
-<p>Thank you! Your instructor will review your submission and update your grade.</p>
-`);
-          });
-        }
-      }
-    }
+   }
   })();
