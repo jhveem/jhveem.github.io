38837140a0c37ae0868928b8d0053ac52c71a929
jhveem <joshua.hveem@gmail.com>
2020-05-19 09:13:33 -0600

    select front page

--- custom_features/modules/display_front_page.js
+++ custom_features/modules/display_front_page.js
@@ -3,6 +3,16 @@ if (/^\/courses\/[0-9]+\/modules/.test(window.location.pathname)) {
   let rPieces = /^\/courses\/([0-9]+)\/modules/;
   let pieces = window.location.pathname.match(rPieces);
   let courseId = parseInt(pieces[1]);
+  //get header on modules page and add an empty div
+  let moduleModal = $(".header-bar");
+  let moduleHeader = $("<div></div>");
+  moduleModal.after(moduleHeader);
+  if (IS_TEACHER) {
+    moduleHeader.append("<select></select>");
+    $.get("/api/v1/courses"+courseId+"/pages").done(function(data) {
+      console.log(data);
+    });
+  }
   $.get("/api/v1/courses/"+courseId+"/pages/btech-custom-settings", function(data) {
     //if custom settings page exists, look for the appropriate header
     $('body').append("<settings id='btech-custom-settings'></settings>");
@@ -10,11 +20,6 @@ if (/^\/courses\/[0-9]+\/modules/.test(window.location.pathname)) {
     settings.html(data.body);
     let page = settings.find('#modules-page-header');
     if (page.length > 0) {
-      //get header on modules page and add an empty div
-      let moduleModal = $(".header-bar");
-      let frontPage = $("<div></div>");
-      moduleModal.after(frontPage);
-
       //get the name of the page to append and then grab the page
       let pageName = page.text();
       $.get("/api/v1/courses/" + courseId + "/pages/" + pageName, function (data) {
