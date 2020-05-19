6dfd2420a7f99259191171d084b08f5089ed8359
jhveem <joshua.hveem@gmail.com>
2020-05-19 08:43:12 -0600

    fix ref to settings page

--- custom_features/modules/display_front_page.js
+++ custom_features/modules/display_front_page.js
@@ -3,7 +3,7 @@ if (/^\/courses\/[0-9]+\/modules/.test(window.location.pathname)) {
   let rPieces = /^\/courses\/([0-9]+)\/modules/;
   let pieces = window.location.pathname.match(rPieces);
   let courseId = parseInt(pieces[1]);
-  $.get("/api/v1/courses/"+courseId+"/btech-custom-settings", function(data) {
+  $.get("/api/v1/courses/"+courseId+"/pages/btech-custom-settings", function(data) {
     //if custom settings page exists, look for the appropriate header
     let body = data.body;
     let page = $(body).find('#modules-page-header');
