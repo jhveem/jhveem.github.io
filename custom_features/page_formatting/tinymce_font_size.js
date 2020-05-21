228e75cf8e498553bee57344a1f79a8185b2bc50
jhveem <joshua.hveem@gmail.com>
2020-05-21 14:26:39 -0600

    trying to fix headers again

--- custom_features/page_formatting/tinymce_font_size.js
+++ custom_features/page_formatting/tinymce_font_size.js
@@ -10,6 +10,6 @@ async function _init() {
   let editor = await getEditor();
   //use rem instead of pixels because it messes everything up otherwise. 1.5, 1.2, 1 for h2, h3, h4 respectively
   //These should work out to 18, 14, and 12
-  tinyMCE.activeEditor.iframeElement.contentDocument.getElementsByTagName('style')[1].innerHTML = tinyMCE.activeEditor.iframeElement.contentDocument.getElementsByTagName('style')[1].innerHTML + "h2 {font-size: 1.5rem; font-weight: bold;} h3 {font-size: 1.2rem; font-weight: bold;} h4 {font-size: 1rem; font-weight: bold;}";
+  tinyMCE.activeEditor.iframeElement.contentDocument.getElementsByTagName('style')[0].innerHTML = tinyMCE.activeEditor.iframeElement.contentDocument.getElementsByTagName('style')[0].innerHTML + "h2 {font-size: 1.5rem; font-weight: bold;} h3 {font-size: 1.2rem; font-weight: bold;} h4 {font-size: 1rem; font-weight: bold;}";
 }
 _init();
