f6c39f1170e970c7d33f2a62b9fda770a1b1ef65
jhveem <joshua.hveem@gmail.com>
2020-06-01 10:18:41 -0600

    removing some divs that may or may not be needed

--- custom_features/reports/grades_report.vue
+++ custom_features/reports/grades_report.vue
@@ -1,25 +1,24 @@
 <template>
-  <div id='btech-modal'>
-    <div class='btech-modal-content' id='btech-modal-content'>
-      <span class='btech-close' id='btech-close'>&times;</span>
-      <h3 style='text-align: center;'>Report</h3>
-      <h5 style='text-align: center;'>Click on column headers to sort by that column.</h5>
-      <h5 style='text-align: center;'>Hover over column headers for a description of the information displayed in that column.</h5>
-      <div id='btech-report-options'></div>
-      <table class='sortable' border='1' id='btech-report-table'>
+  <div>
+    <span class='btech-close' id='btech-close'>&times;</span>
+    <h3 style='text-align: center;'>Report</h3>
+    <h5 style='text-align: center;'>Click on column headers to sort by that column.</h5>
+    <h5 style='text-align: center;'>Hover over column headers for a description of the information displayed in that
+      column.</h5>
+    <div id='btech-report-options'></div>
+    <table class='sortable' border='1' id='btech-report-table'>
 
-      </table>
-      <thead border='1' id='btech-report-table-head'>
-        <tr>
-          <th v-for='column in columns'>test</th>
-        </tr>
-      </thead>
-      <tbody border='1' id='btech-report-table-body'>
+    </table>
+    <thead border='1' id='btech-report-table-head'>
+      <tr>
+        <th v-for='column in columns'>test</th>
+      </tr>
+    </thead>
+    <tbody border='1' id='btech-report-table-body'>
 
-      </tbody>
-      <tfoot border='1' id='btech-report-table-foot'>
+    </tbody>
+    <tfoot border='1' id='btech-report-table-foot'>
 
-      </tfoot>
-    </div>
-  </div> 
+    </tfoot>
+  </div>
 </template>
