77ecb4fabfbff0d1aeb7ec56499d3569d749a1db
jhveem <joshua.hveem@gmail.com>
2020-06-01 10:17:29 -0600

    testing

--- custom_features/reports/grades_report.vue
+++ custom_features/reports/grades_report.vue
@@ -11,7 +11,7 @@
       </table>
       <thead border='1' id='btech-report-table-head'>
         <tr>
-          <th v-for='column in columns'>{{column.name}}</th>
+          <th v-for='column in columns'>test</th>
         </tr>
       </thead>
       <tbody border='1' id='btech-report-table-body'>
