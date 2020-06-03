06a9593876127de505c938e1fec96d24ea8eccd9
jhveem <joshua.hveem@gmail.com>
2020-06-03 11:13:02 -0600

    no more components

--- custom_features/reports/grades_report.vue
+++ custom_features/reports/grades_report.vue
@@ -6,13 +6,22 @@
       <h5 style='text-align: center;'>Click on column headers to sort by that column.</h5>
       <h5 style='text-align: center;'>Hover over column headers for a description of the information displayed in that
         column.</h5>
-      <div>
-        <div style='display: inline-block; padding: 4px;' v-for='column in columns' :key='column.name' :class='column.sortable_type'>{{column.name}}</div>
-      </div>
-      <div>
-        <div v-for='student in students'>
-          <div v-for='column in columns' :key='column.name' style='display: inline-block;'>{{columnNameToCode(column.name)}}</div>
-        </div>
-      </div>
+      <div id='btech-report-options'></div>
+      <table class='sortable' border='1' id='btech-report-table'>
+        <thead border='1'>
+          <tr>
+            <th v-for='column in columns' :key='column.name' :class='column.sortable_type'>{{column.name}}</th>
+          </tr>
+        </thead>
+        <tbody border='1'>
+          <tr v-for='(student, id) in students'>
+            <td v-for='column in columns' :key='column.name' v-bind:style="{'background-color': getDaysSinceLastSubmissionColor(column.name, student[column.name.toLowerCase().replace(/ /g, '_')])}">{{getColumnText(column, student[column.name.toLowerCase().replace(/ /g, "_")])}}</td>
+          </tr>
+        </tbody>
+        <tfoot border='1'>
+
+        </tfoot>
+      </table>
+    </div>
   </div>
 </template>
