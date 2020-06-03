c703aae99123645e74c0e9826107185871c27ae9
jhveem <joshua.hveem@gmail.com>
2020-06-03 10:05:31 -0600

    move report row into component

--- custom_features/reports/grades_report.vue
+++ custom_features/reports/grades_report.vue
@@ -7,7 +7,7 @@
       <h5 style='text-align: center;'>Hover over column headers for a description of the information displayed in that
         column.</h5>
       <div>
-        <div v-for='column in columns' :key='column.name' :class='column.sortable_type'>{{column.name}}</div>
+        <div style='display: inline-block; padding: 4px;' v-for='column in columns' :key='column.name' :class='column.sortable_type'>{{column.name}}</div>
       </div>
       <div>
         <report-row v-for='(student,  id) in students'
