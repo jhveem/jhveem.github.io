66ac23fa6780f329b77d68b6d19fcff76a7d7edc
jhveem <joshua.hveem@gmail.com>
2020-06-04 09:48:17 -0600

    add reports css

--- custom_features/reports/individual_page/template.vue
+++ custom_features/reports/individual_page/template.vue
@@ -7,8 +7,8 @@
       <h5 style='text-align: center;'>Click on column headers to sort by that column.</h5>
       <h5 style='text-align: center;'>Hover over column headers for a description of the information displayed in that
         column.</h5>
-      <div id='btech-report-options'>
-        <div style='display: inline-block;' v-for='column in columns' :key='column.name'><input type="checkbox" v-model="column.visible"><label>{{column.name}}</label></div>
+      <div class='btech-report-columns-toggle'>
+        <div class='btech-report-column-toggle' style='display: inline-block;' v-for='column in columns' :key='column.name'><input type="checkbox" v-model="column.visible"><label>{{column.name}}</label></div>
       </div>
       <table class='sortable' border='1'>
         <thead border='1'>
