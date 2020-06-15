ab7530f25c5ab43d3c8285c8c227de0c2c539d31
jhveem <joshua.hveem@gmail.com>
2020-06-15 12:00:36 -0600

    open form in iframe

--- custom_features/survey/survey.js
+++ custom_features/survey/survey.js
@@ -90,9 +90,10 @@ style="text-align:left;color:#666;border-bottom:1px solid #d3d8d3;padding:0;min-
   let form = $(`
 <form
 action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSffvo5Uap6vY_DFz8x9nv7Evo7OnczEOtJXqWyzLNBnlmOhZQ/formResponse"
-method="POST" id="m_8914134288611702631ss-form" target="_blank">
+method="POST" id="m_8914134288611702631ss-form" target="formSubmitFrame">
 </form>
 `);
+form.after("<iframe name='formSubmitFrame' title='holds submitted form data' rel='nofollow' class='btech-hidden'></iframe>");
   let courseId = ENV.COURSE_ID;
   let userId = ENV.current_user.id;
   $(".btech-survey").append(form);
