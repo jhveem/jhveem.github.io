cf6276f066a2f57ebc4f5f8e5b4d1853faa0980c
jhveem <joshua.hveem@gmail.com>
2020-06-24 14:01:03 -0600

    center text

--- custom_features/welcome_banner.js
+++ custom_features/welcome_banner.js
@@ -2,6 +2,6 @@ if (window.location.pathname === "/") {
   console.log("WELCOME!");
   let main = $("#main");
   main.prepend(`
-<div style="width: 100%; padding: 20px; background: var(--ic-brand-global-nav-bgd); color: #FFFFFF; font-size: 1.5em;">Welcome to Bridgerland. If you need help getting started, visit the <a href="/courses/480103" target="_blank" style="color: #FFF; cursor: pointer; text-decoration: underline;">Student Orientation</a> course.</div>
+<div style="text-align: center; width: 100%; padding: 20px; background: var(--ic-brand-global-nav-bgd); color: #FFFFFF; font-size: 1.5em;">Welcome to Bridgerland. If you need help getting started, visit the <a href="/courses/480103" target="_blank" style="color: #FFF; cursor: pointer; text-decoration: underline;">Student Orientation</a> course.</div>
 `);
 }
