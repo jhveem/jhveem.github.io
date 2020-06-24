269ca9edd0129b603e7b94ed6029f48bc48b558a
jhveem <joshua.hveem@gmail.com>
2020-06-24 14:15:24 -0600

    clean up welcome banner feature

--- custom_features/welcome_banner.js
+++ custom_features/welcome_banner.js
@@ -1,7 +1,4 @@
-if (window.location.pathname === "/") {
-  console.log("WELCOME!");
-  let main = $("#main");
-  main.prepend(`
-<div style="text-align: center; width: 100%; padding: 15px; background: var(--ic-brand-global-nav-bgd); color: #FFFFFF; font-size: 1.25em;">Welcome to Bridgerland. If you need help getting started, visit the <a href="/courses/480103" target="_blank" style="color: #FFF; cursor: pointer; text-decoration: underline;">Student Orientation</a> course.</div>
+let main = $("#main");
+main.prepend(`
+  <div style="text-align: center; width: 100%; padding: 15px; background: var(--ic-brand-global-nav-bgd); color: #FFFFFF; font-size: 1.25em;">Welcome to Bridgerland. If you need help getting started, visit the <a href="/courses/480103" target="_blank" style="color: #FFF; cursor: pointer; text-decoration: underline;">Student Orientation</a> course.</div>
 `);
-}
