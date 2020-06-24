f2373f651992344c6ba362e20a9b0f816eab92d6
jhveem <joshua.hveem@gmail.com>
2020-06-24 14:07:18 -0600

    narrower welcome banner

--- custom_features/welcome_banner.js
+++ custom_features/welcome_banner.js
@@ -1,7 +1,7 @@
 if (window.location.pathname === "/") {
   console.log("WELCOME!");
-  let main = $("#main");
+  let main = $("main");
   main.prepend(`
-<div style="text-align: center; width: 100%; padding: 20px; background: var(--ic-brand-global-nav-bgd); color: #FFFFFF; font-size: 1.5em;">Welcome to Bridgerland. If you need help getting started, visit the <a href="/courses/480103" target="_blank" style="color: #FFF; cursor: pointer; text-decoration: underline;">Student Orientation</a> course.</div>
+<div style="text-align: center; width: 100%; padding: 15px; background: var(--ic-brand-global-nav-bgd); color: #FFFFFF; font-size: 1.25em;">Welcome to Bridgerland. If you need help getting started, visit the <a href="/courses/480103" target="_blank" style="color: #FFF; cursor: pointer; text-decoration: underline;">Student Orientation</a> course.</div>
 `);
 }
