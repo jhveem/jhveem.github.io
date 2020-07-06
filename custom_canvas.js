2fc9fa48d9e922b057a7c60e410934b576ccb323
jhveem <joshua.hveem@gmail.com>
2020-07-06 12:39:43 -0600

    feature to transfer all users in a section to another course

--- custom_canvas.js
+++ custom_canvas.js
@@ -305,6 +305,9 @@ $.getScript("https://cdn.jsdelivr.net/npm/vue").done(function () {
           if (departmentId === 3827 || departmentId === 3828) { //Practical Nursing && Health Sciences General
             feature("editor_toolbar/syllabi", {}, /^\/courses\/[0-9]+\/(pages|assignments|quizzes)/);
           }
+          if (currentUser === 451624) { //Yvonne
+            feature("department_specific/nursing_transfer_sections", {}, /^\/courses\/[0-9]+\/users$/);
+          }
           if (departmentId === 3833) { //business
             feature("department_specific/business_hs");
             feature("previous-enrollment-data/previous_enrollment_period_grades");
---
+++ custom_features/department_specific/nursing_transfer_sections.js
@@ -0,0 +1,31 @@
+let div = $("<div></div>");
+$(".v-gutter").before(div);
+let sectionSelector = $("<select><option selected disabled>-Section to Transfer-</option></select>");
+let courseSelector = $("<input type='text' placeholder='Enter Course Id'>");
+let submit = $("<a href='#' class='btn btn-primary' role='button'>Transfer</a>");
+let sectionData = {};
+div.append(sectionSelector);
+div.append(courseSelector);
+div.append(submit);
+submit.click(function () {
+  let section = sectionData[sectionSelector.val()];
+  let courseId = courseSelector.val();
+  for (let s = 0; s < section.students.length; s++) {
+    let student = section.students[s];
+    $.post("/api/v1/courses/" + courseId + "/enrollments", {
+      enrollment: {
+        user_id: student.id,
+        type: "StudentEnrollment"
+      }
+    });
+  }
+});
+let sections = $.get("/api/v1/courses/511568/sections?include[]=students").done(function (data) {
+  for (let s = 0; s < data.length; s++) {
+    let section = data[s];
+    console.log(section);
+    let option = $("<option value='" + section.id + "'>" + section.name + "</option>");
+    sectionData[section.id] = section;
+    sectionSelector.append(option);
+  }
+});
