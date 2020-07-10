7c071aedb077fc98e478db53645cda5c555d97e8
jhveem <joshua.hveem@gmail.com>
2020-07-10 15:18:00 -0600

    fix multiple submissions

--- custom_features/department_specific/esthetics_cosmetology_services.js
+++ custom_features/department_specific/esthetics_cosmetology_services.js
@@ -224,7 +224,7 @@
                           };
                         }
                         this.loading = true;
-                        for (let i = 1; i < this.criterionNumber; i++) {
+                        for (let i = 1; i <= this.criterionNumber; i++) {
                           await $.put(url, {
                             comment: {
                               text_comment: this.createComment(service, this.reviewerComment)
