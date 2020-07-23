ddae99278eed9b9dd52a860fb20fa692a4b700b4
Joshua Hveem <joshua.hveem@gmail.com>
2020-07-22 21:02:26 -0600

    outline goal code

--- custom_features/editor_toolbar/html_practice.js
+++ custom_features/editor_toolbar/html_practice.js
@@ -14,7 +14,7 @@
       });
       $(this).after(input);
       input.after(display);
-      let correct = $("<div style='width: 100%; background-color: #2B3; color: #FFF; text-align: center; font-size: 1.5em;'>Correct!</div>");
+      let correct = $("<div style='width: 100%; background-color: #2B3; color: #FFF; text-align: center; font-size: 1em;'>Correct!</div>");
       correct.hide();
       $(this).after(correct);
       $(this).after("<div style='outline: 1px solid #000; padding: 5px; margin-bottom: 10px;'>" + html + "</div>");
