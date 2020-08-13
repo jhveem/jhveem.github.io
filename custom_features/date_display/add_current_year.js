97295982502f98f57725fe69789ee308ab5fe638
jhveem <joshua.hveem@gmail.com>
2020-08-13 10:09:55 -0600

    fix ref to iframe to change year

--- custom_features/date_display/add_current_year.js
+++ custom_features/date_display/add_current_year.js
@@ -6,7 +6,7 @@ IMPORTED_FEATURE = {
     feature.setAssignmentSubmittedDateHeader("span.submission-details-header__time");
     //this has been commented out because it's breaking the grade updater. Will have to readdress this if we need to add it back in.
     // feature.setAssignmentSubmittedDateHeader("div.quiz-submission.headless", "#preview_frame");
-    let preEl = await getElement("div.quiz-submission.headless .quiz_score", "#preview_frame");
+    let preEl = await getElement("div.quiz-submission.headless .quiz_score", "#iframe_holder iframe");
     let header = preEl.next();
     feature.setAssignmentSubmittedDateHeaderElement(header);
     feature.setAssignmentSubmittedDateHeader("div.comment_list span.posted_at");
